37196fab82f4c4ca305fe97162f3dd6200b835c3
ShuzZzle <niclas.schad@gmail.com>
2019-09-08 21:17:16 +0200

    First Commit

---
+++ src/main/java/com/shuzzzle/mercurii/MercuriiApplication.java
@@ -0,0 +1,28 @@
+package com.shuzzzle.mercurii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class MercuriiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuriiApplication.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mercurii");
+        dataSource.setUsername("root");
+        dataSource.setPassword("test");
+        return dataSource;
+    }
+
+}
